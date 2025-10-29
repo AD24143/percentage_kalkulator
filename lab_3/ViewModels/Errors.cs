612e49d91f73ec8238162cf67ac532ef23c97277
Anastasiya Dziamchuk <dziamchuk24143@stud.akademiabialska.pl>
2025-10-29 15:30:43 +0100

    added body to errors

--- lab_3/ViewModels/Errors.cs
+++ lab_3/ViewModels/Errors.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace lab_3
 {
-    class Errors
+   public class Errors : ICommand
     {
+
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public Errors(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
     }
 }
